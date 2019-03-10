1305bb8a04b2137e7ea06b9a08a7347f28444821
Tanju Yildiz <ben@tanjuyildiz.com>
2019-03-10 19:21:48 +0300

    Added clean task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const gulp = require('gulp')
 const autoprefixer = require('gulp-autoprefixer')
 const cssnano = require('gulp-cssnano')
 const concat = require('gulp-concat')
+const del = require('del')
 const notify = require('gulp-notify')
 const plumber = require('gulp-plumber')
 const rename = require('gulp-rename')
@@ -40,61 +41,91 @@ const onError = function(err) {
 // TASKS
 //
 
-// Task: 'styles'
-gulp.task('styles', done => {
-  gulp
-    .src(paths.styles.input)
-    .pipe(
-      plumber({
-        errorHandler: onError
-      })
-    )
-    .pipe(
-      sass({
-        outputStyle: 'expanded'
-      })
-    )
-    .pipe(
-      autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-      })
-    )
-    .pipe(gulp.dest(paths.styles.output))
-    .pipe(cssnano())
-    .pipe(
-      rename({
-        suffix: '.min'
-      })
-    )
-    .pipe(gulp.dest(paths.styles.output))
+/**
+ * Task: 'clean'
+ *
+ * Remove pre-existing content from output folders
+ */
+gulp.task(
+  'clean',
+  gulp.series(done => {
+    // Clean the dist folder
+    del.sync(['dist/'])
 
-  // Signal completion
-  done()
-})
+    // Signal completion
+    done()
+  })
+)
 
-// Task: 'scripts'
-gulp.task('scripts', done => {
-  gulp
-    .src(paths.scripts.input)
-    .pipe(
-      plumber({
-        errorHandler: onError
-      })
-    )
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest(paths.scripts.output))
-    .pipe(uglify())
-    .pipe(
-      rename({
-        suffix: '.min'
-      })
-    )
-    .pipe(gulp.dest(paths.scripts.output))
+/**
+ * Task: 'styles'
+ *
+ * Compile, autoprefix  & minify SASS files
+ */
+gulp.task(
+  'styles',
+  gulp.series(done => {
+    gulp
+      .src(paths.styles.input)
+      .pipe(
+        plumber({
+          errorHandler: onError
+        })
+      )
+      .pipe(
+        sass({
+          outputStyle: 'expanded'
+        })
+      )
+      .pipe(
+        autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+        })
+      )
+      .pipe(gulp.dest(paths.styles.output))
+      .pipe(cssnano())
+      .pipe(
+        rename({
+          suffix: '.min'
+        })
+      )
+      .pipe(gulp.dest(paths.styles.output))
 
-  // Signal completion
-  done()
-})
+    // Signal completion
+    done()
+  })
+)
+
+/**
+ * Task: 'scripts'
+ *
+ * Concanate & minify JavaScript files
+ */
+gulp.task(
+  'scripts',
+  gulp.series(done => {
+    gulp
+      .src(paths.scripts.input)
+      .pipe(
+        plumber({
+          errorHandler: onError
+        })
+      )
+      .pipe(concat('scripts.js'))
+      .pipe(gulp.dest(paths.scripts.output))
+      .pipe(uglify())
+      .pipe(
+        rename({
+          suffix: '.min'
+        })
+      )
+      .pipe(gulp.dest(paths.scripts.output))
+
+    // Signal completion
+    done()
+  })
+)
 
 // Build
 gulp.task('build', gulp.series(['styles', 'scripts']))
@@ -106,4 +137,4 @@ gulp.task('watch', () => {
 })
 
 // Default
-gulp.task('default', gulp.series(['build', 'watch']))
+gulp.task('default', gulp.series(['clean', 'build', 'watch']))
