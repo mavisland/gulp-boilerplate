c093de6c8b1cb0680ef7eecf2f2f94ee6926ca8b
Tanju Yildiz <ben@tanjuyildiz.com>
2019-03-10 16:14:50 +0300

    Added styles task

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 A boilerplate for building web projects with [Gulp](https://gulpjs.com/) 4.x.
 
-## Quick Start
+## Getting Started
+
+### Features
+
+- Compile, minify, autoprefix SASS files.
+- Watch for file changes, and automatically recompile build.
+
+### Quick Start
 
 ```
 # 1 Clone this repo
@@ -16,6 +23,45 @@ npm install
 
 # 4 Get started
 gulp
+```
+
+### Requirements
+
+This project requires you have [nodejs](https://nodejs.org/en/) with [npm](https://www.npmjs.com/get-npm) installed.
+This project requires you have a global installation of [gulp](http://gulpjs.com/).
+
+```
+# Install gulp globally
+npm install -g gulp
+npm install -g gulp-cli
+```
+
+## Documentation
+
+Add your source files to the appropriate `src` subdirectories. Gulp will process and and compile them into `dist`.
+
+### Styles
+
+Files in the `src/styles` directory will be compiled to `dist/css`.
+
+## Options
+
+### Paths
+
+Adjust the `input`, `output`, `watch` paths for all of the Gulp tasks under the `paths` variable. Paths are relative to the root project folder.
+
+```js
+/**
+ * Paths to project folders
+ */
+// Paths
+const paths = {
+  styles: {
+    input: 'src/styles/style.scss',
+    output: 'dist/css/style.css',
+    watch: 'src/styles/**/*.scss'
+  }
+}
 ```
 
 ## License
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,78 @@
 'use strict'
 
+// Packages
 const gulp = require('gulp')
+const autoprefixer = require('gulp-autoprefixer')
+const cssnano = require('gulp-cssnano')
+const notify = require('gulp-notify')
+const plumber = require('gulp-plumber')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
+
+// Paths
+const paths = {
+  styles: {
+    input: 'src/styles/style.scss',
+    output: 'dist/css/style.css',
+    watch: 'src/styles/**/*.scss'
+  }
+}
+
+// On Error Configuration
+const onError = function(err) {
+  notify.onError({
+    title: 'Gulp',
+    subtitle: 'Failure!',
+    message: 'Error: <%= error.message %>',
+    sound: 'Beep'
+  })(err)
+  this.emit('end')
+}
+
+//
+// TASKS
+//
+
+// Styles
+gulp.task('styles', done => {
+  gulp
+    .src(paths.styles.input)
+    .pipe(
+      plumber({
+        errorHandler: onError
+      })
+    )
+    .pipe(
+      sass({
+        outputStyle: 'expanded'
+      })
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest(paths.styles.output))
+    .pipe(cssnano())
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(gulp.dest(paths.styles.output))
+
+  // Signal completion
+  done()
+})
+
+// Build
+gulp.task('build', gulp.series('styles'))
+
+// Watch
+gulp.task('watch', () => {
+  gulp.watch(paths.styles.watch, gulp.series('styles'))
+})
+
+// Default
+gulp.task('default', gulp.series(['build', 'watch']))
